2e5a37eca04d02ea1a9dfce460692e11b0705eac
aibek ziyashev <aibek.ziyashev@gmail.com>
2018-01-26 13:41:31 +0600

    smth

--- src/main/java/com/example/wordcounter/App.java
+++ src/main/java/com/example/wordcounter/App.java
@@ -8,6 +8,20 @@ public class App
     }
     
     public static int wordcount(String s) {
-        return 0;
+        String[] arr = s.split("\\W+");
+        int ans = 0;
+        for (String i: arr){
+            int res = 0;
+            for (int j = 0; j < i.length(); j++){
+                char t = i.charAt(j);
+                if ( t < 65 || (t > 90 && t < 97) || t > 122){
+                    res = 1;
+                }
+            }
+            if (res == 0){
+                ans++;
+            }
+        }
+        return ans;
     }
 }
